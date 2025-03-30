3e71f2a638670af597bdfd946e9b0302af909976
Juan Sebastian <129484369+JSebastianL17@users.noreply.github.com>
2025-03-30 16:10:21 -0700

    class and draw line

--- claudia_calculator.cpp
+++ claudia_calculator.cpp
@@ -3,3 +3,14 @@
 #include <string>
 #include <limits>
 #include <spdlog/spdlog.h>
+
+class ClaudiaCalc {
+    private:
+        double a_number = 0.000;
+        double b_number = 0.000;
+        double c_number = 0.000;
+        double d_number = 0.000;
+        
+        void drawLine() const {
+            std::cout << std::string(114, '-') << std::endl;
+        }
