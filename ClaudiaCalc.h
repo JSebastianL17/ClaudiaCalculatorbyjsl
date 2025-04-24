3387b9d0a1daf5dd0d1ac5f795470bac402cfd39
Juan Sebastian <129484369+JSebastianL17@users.noreply.github.com>
2025-04-23 21:40:46 -0700

    new name variables

--- ClaudiaCalc.h
+++ ClaudiaCalc.h
@@ -1,17 +1,14 @@
-// ClaudiaCalc.h
 #ifndef CLAUDIACALC_H
 #define CLAUDIACALC_H
-
-#include <string>
 
 const int LINE_WIDTH = 114;
 
 class ClaudiaCalc {
 private:
-    double a_number;
-    double b_number;
-    double c_number;
-    double d_number;
+    double a;
+    double b;
+    double c;
+    double d;
 
     void drawLine() const;
     void displayRegisters() const;
@@ -19,8 +16,8 @@ private:
     double& getRegister(char reg);
 
 public:
-    ClaudiaCalc(); 
-    void run();    
+    ClaudiaCalc();
+    void run();
 };
 
 #endif
